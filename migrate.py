7c246351bd8437d73b568a0bdc72a277d4cbc26c
mananorth122 <kitazawa.mana@gmail.com>
2026-09-10 17:47:03 +0900

    migrate 2026-7 day-log

--- migrate.py
+++ migrate.py
@@ -8,7 +8,7 @@ from bs4 import BeautifulSoup
 # ==========================================
 # 処理対象の年月を指定
 # ==========================================
-TARGET_YEAR_MONTH = "2026-6"
+TARGET_YEAR_MONTH = "2026-7"
 
 SKIP_ENTRIES = []
 
